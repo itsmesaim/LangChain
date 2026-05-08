f43ff0d628ab3a79a927145653387b70db11b69a
itsmesaim <saimkaskar69@gmail.com>
2026-05-08 12:11:27 +0100

    hello world and environment setups

---
+++ Project 1/main.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+# import os
+load_dotenv()
+
+
+
+def main():
+    print("Hello from project-1!")
+    # print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
