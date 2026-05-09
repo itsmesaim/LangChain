7bc800dd5e61637076353e772aecb7eac8d91c44
root <root@vmi3101055.contaboserver.net>
2026-05-09 21:12:46 +0200

    without @tool

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
-# from tavily import TavilyClient
-from tavily import TavilyClient
-
-from dotenv import load_dotenv
-load_dotenv()
+from langchain_tavily import TavilySearch
 
 
-tavily = TavilyClient()
+llm = ChatOpenAI(model="gpt-4o-mini")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool for search on internet
-    Args:
-        query: the query to search for 
-    returns: 
-        the search result
-    """
-    print(f"searching for{query}")
-    return tavily.search(query=query)
-
-llm= ChatOpenAI(model="gpt-4o-mini")
-tools= [search]
-agent= create_agent(model= llm, tools=tools)
 
 def main():
-    print("Hello from my-learning!")
-    result = agent.invoke({"messages":HumanMessage(content="search for best halal food option in dublin tallaht. i want to have a pizza.")})
+    print("Hello from langchain-course!")
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
+            )
+        }
+    )
     print(result)
+
 
 if __name__ == "__main__":
     main()
